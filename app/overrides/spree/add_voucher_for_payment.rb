e5afa12b9b6258b94427093646fc0b985e4b0ab6
Edward Yang <yushine999@gmail.com>
2015-11-13 21:51:10 +0800

    change voucher to voucher_code.

--- app/overrides/spree/add_voucher_for_payment.rb
+++ app/overrides/spree/add_voucher_for_payment.rb
@@ -2,5 +2,5 @@ Deface::Override.new(
   virtual_path:  'spree/checkout/_payment',
   name:          'add_pay_by_voucher',
   insert_after:  '#payment-methods, [data-hook="payment-methods"]',
-  partial:       'spree/checkout/payment/voucher.html.erb'
+  partial:       'spree/checkout/payment/voucher_code.html.erb'
 )
